2da893be8201def3bda1f92feb81999d9054b2b0
Luca Nogueira de Souza <lucanogueira.desouza@gmail.com>
2025-12-05 15:13:12 -0300

    Add files via upload

---
+++ Bootcamp Santander - Desafios/desafio5.js
@@ -0,0 +1,36 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+
+        switch (this.tipo) {
+            case "mago":
+                ataque = "magia";
+                break;
+            case "guerreiro":
+                ataque = "espada";
+                break;
+            case "monge":
+                ataque = "artes marciais";
+                break;
+            case "ninja":
+                ataque = "shuriken";
+                break;
+            default:
+                ataque = "um ataque desconhecido";
+        }
+
+        console.log(`o ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+const heroi1 = new Heroi("Arthus", 30, "guerreiro");
+const heroi2 = new Heroi("Merlin", 150, "mago");
+
+heroi1.atacar();
+heroi2.atacar();
